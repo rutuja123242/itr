8717c82155be337587ab5ba8e55dd4b40b534cef
rutuja123242 <rutujapatil981@gmail.com>
2024-08-05 19:03:57 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<meta charset="UTF-8">
+
+<title>Classic Events Studio : Homepage </title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta name="keywords" content="solapur tournaments,solapur alleventinfo,cultural events,social ,technical,sports events,competitions in solapur" />
+	
+	<meta name="description" content="Solapur all Events Tournaments information "/>
+	<meta name="robots" content="index">
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css"/>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css"/>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-utilities.css"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.esm.js"></script>
+
+</head>
+<body>
+<?php
+  require_once('header.php');
+?>
+<div class="Mid_container" style="min-height:600px;">
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="assets/images/cultural.png" class="d-block w-100" alt="..." style="height:600;">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/images/sports.png" class="d-block w-100" alt="..." style="height:600;">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/images/technical.png" class="d-block w-100" alt="..." style="height:600;">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/images/social.png" class="d-block w-100" alt="..." style="height:600;">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  </div>
+ <?php
+ require_once('footer.php');
+ ?>
+
+</body>
+</html>
